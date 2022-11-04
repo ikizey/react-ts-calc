c93a5438dbe2d5ea05f8228c818f53aed18b0fdf
kizey <ikizey@icloud.com>
2022-11-04 18:13:41 +0400

    move resultReducer to own file.

--- src/App.tsx
+++ src/App.tsx
@@ -4,105 +4,10 @@ import './App.css';
 import CButton from './components/CButton';
 import Output from './components/Output';
 import { buttons } from './data/buttons';
-
-const initialResult = '0';
-
-const evaluate = (formula: string) => {
-  if (formula.endsWith('-')) {
-    return formula;
-  }
-  if (formula.endsWith(' ')) {
-    return eval(formula.slice(0, -3)) + formula.slice(-3);
-  }
-
-  const temp = formula.replace('X', '*');
-
-  if (eval(temp) === Infinity) {
-    return '0';
-  }
-  return eval(temp).toString();
-};
-
-const resultReducer = (state: string, action: string) => {
-  switch (action) {
-    case 'AC': {
-      return initialResult;
-    }
-    case '+':
-    case '-':
-    case '/':
-    case 'X': {
-      if (action === '-') {
-        if (state === '0') {
-          return action;
-        } else if (
-          state.split(' ').length === 3 &&
-          state.split(' ').at(2) === ''
-        ) {
-          return state + action;
-        } else if (state.endsWith('-')) {
-          return state;
-        }
-      }
-
-      if (state === '-') {
-        return '0 ' + action + ' ';
-      }
-      if (state.endsWith(' -')) {
-        return state.slice(0, -4) + ' ' + action + ' ';
-      }
-      if (state.endsWith(' ')) {
-        return state.slice(0, -3) + ' ' + action + ' ';
-      }
-      if (state.endsWith(' 0.')) {
-        return state.slice(0, -5) + ' ' + action + ' ';
-      }
-      if (state.endsWith('.')) {
-        return state.slice(0, -1) + ' ' + action + ' ';
-      }
-      if (state.split(' ').length === 3) {
-        return evaluate(state) + ' ' + action + ' ';
-      }
-      return state + ' ' + action + ' ';
-    }
-
-    case '=':
-      return evaluate(state);
-
-    case '1':
-    case '2':
-    case '3':
-    case '4':
-    case '5':
-    case '6':
-    case '7':
-    case '8':
-    case '9':
-    case '0': {
-      if (state === '0') {
-        return action;
-      }
-      return state + action;
-    }
-
-    case '.': {
-      const splitted = state.split(' ');
-      if (
-        (splitted.length === 1 && state.includes('.')) ||
-        (splitted.length === 3 && splitted[2].includes('.'))
-      ) {
-        return state;
-      } else if (state.endsWith(' ')) {
-        return state + '0.';
-      }
-      return state + action;
-    }
-  }
-  return state + action;
-};
+import { resultReducer } from './resultReducer';
 
 function App() {
-  const [result, setResult] = useReducer(resultReducer, initialResult);
+  const [result, setResult] = useReducer(resultReducer, '0');
 
   const clickHandle = (event: MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
---
+++ src/resultReducer.ts
@@ -0,0 +1,95 @@
+const initialResult = '0';
+
+const evaluate = (formula: string) => {
+  if (formula.endsWith('-')) {
+    return formula;
+  }
+  if (formula.endsWith(' ')) {
+    return eval(formula.slice(0, -3)) + formula.slice(-3);
+  }
+
+  const temp = formula.replace('X', '*');
+
+  if (eval(temp) === Infinity) {
+    return '0';
+  }
+  return eval(temp).toString();
+};
+
+export const resultReducer = (state: string, action: string) => {
+  switch (action) {
+    case 'AC': {
+      return initialResult;
+    }
+    case '+':
+    case '-':
+    case '/':
+    case 'X': {
+      if (action === '-') {
+        if (state === '0') {
+          return action;
+        } else if (
+          state.split(' ').length === 3 &&
+          state.split(' ').at(2) === ''
+        ) {
+          return state + action;
+        } else if (state.endsWith('-')) {
+          return state;
+        }
+      }
+
+      if (state === '-') {
+        return '0 ' + action + ' ';
+      }
+      if (state.endsWith(' -')) {
+        return state.slice(0, -4) + ' ' + action + ' ';
+      }
+      if (state.endsWith(' ')) {
+        return state.slice(0, -3) + ' ' + action + ' ';
+      }
+      if (state.endsWith(' 0.')) {
+        return state.slice(0, -5) + ' ' + action + ' ';
+      }
+      if (state.endsWith('.')) {
+        return state.slice(0, -1) + ' ' + action + ' ';
+      }
+      if (state.split(' ').length === 3) {
+        return evaluate(state) + ' ' + action + ' ';
+      }
+      return state + ' ' + action + ' ';
+    }
+
+    case '=':
+      return evaluate(state);
+
+    case '1':
+    case '2':
+    case '3':
+    case '4':
+    case '5':
+    case '6':
+    case '7':
+    case '8':
+    case '9':
+    case '0': {
+      if (state === '0') {
+        return action;
+      }
+      return state + action;
+    }
+
+    case '.': {
+      const splitted = state.split(' ');
+      if (
+        (splitted.length === 1 && state.includes('.')) ||
+        (splitted.length === 3 && splitted[2].includes('.'))
+      ) {
+        return state;
+      } else if (state.endsWith(' ')) {
+        return state + '0.';
+      }
+      return state + action;
+    }
+  }
+  return state + action;
+};
